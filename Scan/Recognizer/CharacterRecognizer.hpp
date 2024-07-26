94c92bc4f87b3a928152b8f351a07eb302a33198
EMCJava <loyusum246807952@gmail.com>
2024-07-27 04:38:15 +0800

    loosen template matching threshold on one and seven

--- Scan/Recognizer/CharacterRecognizer.hpp
+++ Scan/Recognizer/CharacterRecognizer.hpp
@@ -25,13 +25,13 @@ private:
         Templates {
             Template {'%', "percentage.png", cv::Scalar( 255, 248, 240 ), 190},
             Template {'0',          "0.png", cv::Scalar( 255, 255,   0 ), 205},
-            Template {'1',          "1.png", cv::Scalar( 193, 182, 255 ), 240},
+            Template {'1',          "1.png", cv::Scalar( 193, 182, 255 ), 220},
             Template {'2',          "2.png", cv::Scalar( 160, 158,  95 ), 210},
             Template {'3',          "3.png",  cv::Scalar( 80, 127, 255 ), 200},
             Template {'4',          "4.png", cv::Scalar( 169, 169, 169 ), 214},
             Template {'5',          "5.png", cv::Scalar( 107, 183, 189 ), 190},
             Template {'6',          "6.png", cv::Scalar( 204,  50, 153 ), 205},
-            Template {'7',          "7.png", cv::Scalar( 240, 255, 255 ), 220},
+            Template {'7',          "7.png", cv::Scalar( 240, 255, 255 ), 210},
             Template {'8',          "8.png", cv::Scalar( 255, 144,  30 ), 205},
             Template {'9',          "9.png",  cv::Scalar( 47, 255, 173 ), 205},
     };
