dbbeee6c7eabf856150d75c7c3be475420d9164b
EMCJava <loyusum246807952@gmail.com>
2024-07-27 01:55:51 +0800

    Move and rename ResultPlotDetails to PlotCombinationMeta

---
+++ Opt/UI/PlotCombinationMeta.hpp
@@ -0,0 +1,35 @@
+//
+// Created by EMCJava on 7/26/2024.
+//
+
+#pragma once
+
+#include <implot.h>
+
+#include <Opt/FullStats.hpp>
+
+#include <array>
+
+struct PlotCombinationMeta {
+    FloatTy            Value;
+    int                CombinationID;
+    int                CombinationRank;
+    std::array<int, 5> Indices;
+
+    bool operator>( PlotCombinationMeta Other ) const noexcept
+    {
+        return Value > Other.Value;
+    }
+
+    static ImPlotPoint
+    PlotCombinationMetaImPlotGetter( int idx, void* user_data )
+    {
+        return { (double) idx, ( ( (PlotCombinationMeta*) user_data ) + idx )->Value };
+    }
+
+    static ImPlotPoint
+    PlotCombinationMetaSegmentImPlotGetter( int idx, void* user_data )
+    {
+        return { std::round( (double) idx / 2 ), ( ( (PlotCombinationMeta*) user_data ) + idx )->Value };
+    }
+};
