78a4cbfb1b73b3bf87bd7660d3839ec3f0fa6827
EMCJava <loyusum246807952@gmail.com>
2024-08-04 16:13:28 +0800

    Update version string

--- WuWaOptim.cpp
+++ WuWaOptim.cpp
@@ -38,7 +38,7 @@
 #include "Loca/StringArrayObserver.hpp"
 #include "Loca/Loca.hpp"
 
-#define WUWAOPT_VERSION "v1.1.8"
+#define WUWAOPT_VERSION "v1.1.9"
 
 template <class T, class S, class C>
 auto&
