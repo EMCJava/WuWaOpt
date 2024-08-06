d0b77e41b9b332d53de03aab93330c96a2353a7a
EMCJava <loyusum246807952@gmail.com>
2024-08-06 11:18:10 +0800

    Added save load in CharacterPage

--- Opt/UI/Page/CharacterPage.cpp
+++ Opt/UI/Page/CharacterPage.cpp
@@ -3,3 +3,47 @@
 //
 
 #include "CharacterPage.hpp"
+
+#include <fstream>
+#include <ranges>
+
+
+CharacterPage::CharacterPage( )
+{
+    m_CharactersNode = YAML::LoadFile( CharacterFileName );
+}
+
+std::vector<std::string>
+CharacterPage::GetCharacterList( ) const
+{
+    return m_CharactersNode
+        | std::views::transform( []( const YAML::const_iterator::value_type& Node ) {
+               return Node.first.as<std::string>( );
+           } )
+        | std::ranges::to<std::vector>( );
+}
+
+bool
+CharacterPage::LoadCharacter( const std::string& CharacterName )
+{
+    m_ActiveCharacterName = CharacterName;
+    m_ActiveCharacterNode = m_CharactersNode[ m_ActiveCharacterName ];
+    if ( !m_ActiveCharacterNode )
+    {
+        m_ActiveCharacterNode = m_ActiveCharacterConfig = { };
+        SaveCharacters( );
+        return false;
+    }
+
+    m_ActiveCharacterConfig = m_ActiveCharacterNode.as<CharacterConfig>( );
+    return true;
+}
+
+
+void
+CharacterPage::SaveCharacters( )
+{
+    std::ofstream OutFile( CharacterFileName );
+    OutFile << m_CharactersNode;
+    OutFile.close( );
+}
--- Opt/UI/Page/CharacterPage.hpp
+++ Opt/UI/Page/CharacterPage.hpp
@@ -4,7 +4,31 @@
 
 #pragma once
 
+#include <Opt/Config/CharacterConfig.hpp>
+
+#include <yaml-cpp/yaml.h>
+
+#include <vector>
+#include <string>
 
 class CharacterPage
 {
+    static constexpr char CharacterFileName[] = "characters.yaml";
+
+public:
+    CharacterPage( );
+
+    [[nodiscard]] std::vector<std::string> GetCharacterList( ) const;
+
+    bool LoadCharacter( const std::string& CharacterName );
+    void SaveCharacters( );
+
+    [[nodiscard]] auto& GetActiveConfig( ) { return m_ActiveCharacterConfig; }
+
+protected:
+    YAML::Node m_CharactersNode;
+
+    std::string     m_ActiveCharacterName;
+    YAML::Node      m_ActiveCharacterNode;
+    CharacterConfig m_ActiveCharacterConfig;
 };
